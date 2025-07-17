0c4224ead3c816af7a95f1fad376c31a9cf9c4d3
saadashraf-onebitsolutions <saadashraf549233@gmail.com>
2025-07-17 11:09:06 +0500

    Initial commit

---
+++ assets/custom.js
@@ -0,0 +1,13 @@
+
+//   $(document).ready(function () {
+//     $("#start-svg").hover(
+//       function () {
+//         $(this).addClass("hide");
+//         $("#second-svg").addClass("show");
+//       },
+//       function () {
+//         $(this).removeClass("hide");
+//         $("#second-svg").removeClass("show");
+//       }
+//     );
+//   });
